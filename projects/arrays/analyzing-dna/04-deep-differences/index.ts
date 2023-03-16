58ef5f291bc40303b1f0510cd502dcb451abe2c8
Ridhwan Ramadhany <rdhwnrmdhny@gmail.com>
2023-03-16 23:25:34 +0700

    06. Arrays

--- projects/arrays/analyzing-dna/04-deep-differences/index.ts
+++ projects/arrays/analyzing-dna/04-deep-differences/index.ts
@@ -1,2 +1,31 @@
 // Write your deepDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+export function deepDifferences(
+	a: string[][],
+	b: string[][]
+): ((string | undefined)[] | undefined)[] | undefined {
+	if (a.length !== b.length) {
+		return undefined;
+	}
+
+	const diffArray: ((string | undefined)[] | undefined)[] = [];
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i].length !== b[i].length) {
+			diffArray.push(undefined);
+			continue;
+		}
+
+		diffArray.push([]);
+
+		for (let j = 0; j < a[i].length; j++) {
+			if (a[i][j] === b[i][j]) {
+				diffArray[i]?.push(a[i][j]);
+			} else {
+				diffArray[i]?.push(undefined);
+			}
+		}
+	}
+
+	return diffArray;
+}
--- projects/arrays/text-processor/src/index.ts
+++ projects/arrays/text-processor/src/index.ts
@@ -1,2 +1,46 @@
 // Write your alignTexts function here! ✨
 // You'll need to export it so the tests can run it.
+type AlignTextOption = {
+	align?: "left" | "middle" | "right";
+	width: number;
+};
+
+export function alignTexts(
+	texts: string[],
+	options: AlignTextOption
+): string[][] {
+	const alignedText: string[][] = [];
+	const { align = "left", width } = options;
+
+	function formatText(text: string, widthDiff: number) {
+		if (align === "left") {
+			return `${text}${" ".repeat(widthDiff)}`;
+		} else if (align === "right") {
+			return `${" ".repeat(widthDiff)}${text}`;
+		} else {
+			return `${" ".repeat(Math.floor(widthDiff / 2))}${text}${" ".repeat(
+				Math.ceil(widthDiff / 2)
+			)}`;
+		}
+	}
+
+	for (let i = 0; i < texts.length; i++) {
+		if (texts[i].length <= width) {
+			const widthDiff = Math.abs(width - texts[i].length);
+			alignedText.push([formatText(texts[i], widthDiff)]);
+
+			continue;
+		}
+
+		const splitText = texts[i].split(" ");
+		alignedText.push([]);
+
+		for (let j = 0; j < splitText.length; j++) {
+			const widthDiff =
+				width - splitText[j].length < 0 ? 0 : width - splitText[j].length;
+			alignedText[i].push(formatText(splitText[j], widthDiff));
+		}
+	}
+
+	return alignedText;
+}
