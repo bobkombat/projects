f64d180c0de0d4d34532c138c58b464585b52f3d
Ridhwan Ramadhany <rdhwnrmdhny@gmail.com>
2023-05-04 22:57:04 +0700

    08. Class

--- projects/classes/classifying-creatures/03-hamster-helpers/index.ts
+++ projects/classes/classifying-creatures/03-hamster-helpers/index.ts
@@ -1,2 +1,58 @@
 // Write your type and classes here! ✨
 // You'll need to export the classes so the tests can run them.
+
+export type SmallPetFood =
+	| "bugs"
+	| "fruits"
+	| "insects"
+	| "plants"
+	| "seeds"
+	| "vegetables";
+
+export abstract class SmallFurryPet {
+	readonly species: string;
+	protected happiness = 0;
+
+	constructor(species: string) {
+		this.species = species;
+	}
+
+	abstract eats(food: SmallPetFood): boolean;
+
+	isHappy() {
+		return this.happiness > 0;
+	}
+}
+
+export class Gerbil extends SmallFurryPet {
+	constructor() {
+		super("Gerbil");
+	}
+
+	dig() {
+		this.happiness += 1;
+	}
+
+	eats(food: SmallPetFood): boolean {
+		return (
+			food === "insects" ||
+			food === "plants" ||
+			food === "seeds" ||
+			food === "vegetables"
+		);
+	}
+}
+
+export class Hamster extends SmallFurryPet {
+	constructor() {
+		super("Hamster");
+	}
+
+	run() {
+		this.happiness += 1;
+	}
+
+	eats(): boolean {
+		return true;
+	}
+}
--- projects/classes/the-shape-of-types/src/index.ts
+++ projects/classes/the-shape-of-types/src/index.ts
@@ -1,2 +1,72 @@
 // Write your classes here! ✨
 // You'll need to export them so the tests can run them.
+interface Consumed {
+	evil: boolean;
+	name: string;
+	power: number;
+}
+
+export abstract class Horror {
+	#consumed: Consumed[] = [];
+
+	protected abstract readonly name: string;
+
+	#consume(opponent: Horror) {
+		this.#consumed.push({
+			evil: opponent.isEvil(),
+			name: opponent.name,
+			power: opponent.getPower(),
+		});
+	}
+
+	doBattle(Opponent: Horror) {
+		if (this.getPower() >= Opponent.getPower()) {
+			this.#consume(Opponent);
+		}
+	}
+
+	getPower() {
+		return this.#consumed.reduce(
+			(previous, consumed) => previous + this.getPowerFrom(consumed),
+			this.#consumed.length
+		);
+	}
+
+	protected abstract getPowerFrom(consumed: Consumed): number;
+	protected abstract isEvil(): boolean;
+}
+
+export class Demon extends Horror {
+	protected readonly name = "Demon";
+
+	constructor() {
+		super();
+	}
+
+	protected getPowerFrom(consumed: Consumed): number {
+		return consumed.evil ? consumed.power / 2 : consumed.power * 2;
+	}
+
+	protected isEvil(): boolean {
+		return true;
+	}
+}
+
+export class Sorcerer extends Horror {
+	protected readonly name: string;
+	#evil: boolean;
+
+	constructor(name: string, evil: boolean) {
+		super();
+		this.name = name;
+		this.#evil = evil;
+	}
+
+	protected getPowerFrom(consumed: Consumed): number {
+		return consumed.evil === this.#evil ? consumed.power * 2 : consumed.power;
+	}
+
+	protected isEvil(): boolean {
+		return this.#evil;
+	}
+}
